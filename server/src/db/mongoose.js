3fa4e01503782fe873349d69f700b66b9083a026
Kapur Mahatara <61814303+Geosphetic@users.noreply.github.com>
2023-03-17 21:10:25 +0545

    Update mongoose.js

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const conn = mongoose.connect('mongodb+srv://kapoorkshetri97:RKapur77@cluster0.4bnwwpm.mongodb.net/?retryWrites=true&w=majority',{
+const conn = mongoose.connect('mongodb+srv://<DB_USERNAME>:<DB_PASSWORD>@cluster0.4bnwwpm.mongodb.net/?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
