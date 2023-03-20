c9fc089206b4ab07b871e3d033a1aabdd5556723
Kapur Mahatara <61814303+Geosphetic@users.noreply.github.com>
2023-03-20 12:57:17 +0545

    Delete server directory

--- server/src/README.md
+++
@@ -1,1 +0,0 @@
-# Change the <DB_USERNAME> and <DB_PASSWORD> of the Line 3 of ## mangoose.js with your MongoDB Atlas username and password.
--- server/src/db/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const conn = mongoose.connect('mongodb+srv://<DB_USERNAME>:<DB_PASSWORD>@cluster0.4bnwwpm.mongodb.net/?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
