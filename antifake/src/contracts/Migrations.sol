0084e586e880298b4d29db3071029f1604d441d0
Kapur Mahatara <61814303+Geosphetic@users.noreply.github.com>
2023-03-23 09:10:01 +0545

    Update Migrations.sol

--- antifake/src/contracts/Migrations.sol
+++ antifake/src/contracts/Migrations.sol
@@ -3,9 +3,11 @@ pragma solidity >=0.5.16;
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
+  uint public constant MAX_MIGRATION_STEPS = 30; // Maximum number of migration steps
 
   modifier restricted() {
-    if (msg.sender == owner) _;
+    require(msg.sender == owner, "Only the contract owner can call this function.");
+    _;
   }
 
   constructor() public {
@@ -13,6 +15,20 @@ contract Migrations {
   }
 
   function setCompleted(uint completed) public restricted {
+    require(completed <= MAX_MIGRATION_STEPS, "Invalid migration step."); // Check if completed step is valid
     last_completed_migration = completed;
+  }
+
+  function getOwner() public view returns (address) {
+    return owner;
+  }
+
+  function getLastCompletedMigration() public view returns (uint) {
+    return last_completed_migration;
+  }
+
+  function upgrade(address newAddress) public restricted {
+    Migrations upgraded = Migrations(newAddress);
+    upgraded.setCompleted(last_completed_migration);
   }
 }
